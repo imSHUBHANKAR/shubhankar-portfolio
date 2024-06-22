207335bd4de8261778c31a0c4b2cc971f270d13e
SHUBHANKAR KUMAR <shubhankar.kr24@gmail.com>
2024-06-22 15:23:25 +0530

    about us commit

--- src/components/Exprience/Experience.jsx
+++ src/components/Exprience/Experience.jsx
@@ -8,73 +8,75 @@ import {FcOk} from 'react-icons/fc'
 
 const Experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skills I have</h5>
       <h2>My Experience</h2>
 
-      <div className='container experience__container'>
-        <div className='experience__frontend'>
+      <div className="container experience__container">
+        {/* --------------------------------1st sections -------------------------------------------------*/}
+        <div className="experience__frontend">
           <h3>Frontend Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <FcRating className='experience__details-icon' />
+          <div className="experience__content">
+            <article className="experience__details">
+              <FcRating className="experience__details-icon" />
               <div>
                 <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcRating className='experience__details-icon' />
+            <article className="experience__details">
+              <FcRating className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcRating className='experience__details-icon' />
+            <article className="experience__details">
+              <FcRating className="experience__details-icon" />
               <div>
                 <h4>JavaScript</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcRating className='experience__details-icon' />
+            <article className="experience__details">
+              <FcRating className="experience__details-icon" />
               <div>
                 <h4>Bootstrap</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcRating className='experience__details-icon' />
+            <article className="experience__details">
+              <FcRating className="experience__details-icon" />
               <div>
                 <h4>React Js</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
           </div>
         </div>
+        {/* --------------------------------2nd sections -------------------------------------------------*/}
         <div className="experience__backend">
           <h3>Backend Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <FcLandscape className='experience__details-icon' />
+          <div className="experience__content">
+            <article className="experience__details">
+              <FcLandscape className="experience__details-icon" />
               <div>
                 <h4>NodeJs</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcLandscape className='experience__details-icon' />
+            <article className="experience__details">
+              <FcLandscape className="experience__details-icon" />
               <div>
                 <h4>MongoDB</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcLandscape className='experience__details-icon' />
+            <article className="experience__details">
+              <FcLandscape className="experience__details-icon" />
               <div>
                 <h4>PHP</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             {/* <article className='experience__details'>
@@ -84,52 +86,51 @@ const Experience = () => {
               <small className='text-light'>Intermediate</small>
               </div>
             </article> */}
-            <article className='experience__details'>
-              <FcLandscape className='experience__details-icon' />
+            <article className="experience__details">
+              <FcLandscape className="experience__details-icon" />
               <div>
                 <h4>Laravel</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
           </div>
         </div>
-
-        <div className='experience__frontend'>
+        {/* --------------------------------3rd sections -------------------------------------------------*/}
+        <div className="experience__frontend">
           <h3>App Development</h3>
-          <div className='experience__content'>
-            <article className='experience__details'>
-              <FcComments className='experience__details-icon' />
+          <div className="experience__content">
+            <article className="experience__details">
+              <FcComments className="experience__details-icon" />
               <div>
                 <h4>Android App</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcComments className='experience__details-icon' />
+            <article className="experience__details">
+              <FcComments className="experience__details-icon" />
               <div>
                 <h4>Kotlin</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
           </div>
         </div>
-
-        <div className='experience__frontend'>
+        {/* --------------------------------4th sections -------------------------------------------------*/}
+        <div className="experience__frontend">
           <h3>UI/UX Design</h3>
-          <div className='experience__content'>
-
-            <article className='experience__details'>
-              <FcOk className='experience__details-icon' />
+          <div className="experience__content">
+            <article className="experience__details">
+              <FcOk className="experience__details-icon" />
               <div>
                 <h4>User Interface</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <FcOk className='experience__details-icon' />
+            <article className="experience__details">
+              <FcOk className="experience__details-icon" />
               <div>
                 <h4>User Experience</h4>
-                <small className='text-light'>Intermediate</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <div className="aexperience__content-image">
@@ -137,10 +138,9 @@ const Experience = () => {
             </div>
           </div>
         </div>
-
       </div>
     </section>
-  )
+  );
 }
 
 export default Experience
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,39 +20,7 @@ const About = () => {
           </div>
         </div>
 
-        <div className='about__content'>
-          <div className='about__cards'>
-            <article className='about__card'>
-              {/* <FaAward className='about__icon'/> */}
-              <FcApproval className='about__icon'/>
-              <h5>Experience</h5>
-              <small>1+ Years Working</small>
-            </article>
-
-            {/* <article className='about__card'>
-              <FiUsers className='about__icon'/>
-              <h5>Clients</h5>
-              <small>50+ Worldwide</small>
-            </article> */}
-
-            <article className='about__card'>
-              {/* <VscFolderLibrary className='about__icon'/> */}
-              <FcOpenedFolder className='about__icon'/>
-              <h5>Projects</h5>
-              <small>4+ Completed</small>
-            </article>
-
-          </div>
-          <p>
-          Eager to embark on a career in the field of computer science and learn new things and skills in life. 
-          Focus on continuous growth, adaptability, and teamwork. Contribute to an innovative organization's success 
-          while expanding my skills and knowledge. Main area of interest is Web and App development along with drawing, 
-          painting, sketching, which helps to create new ideas and can handle multiple tasks daily.
-          </p>
-
-          <Link to='contact' spy={true} smooth={true} offset={50} duration={500} className='btn btn-primary'>Let's Talk! <FcCustomerSupport/></Link>
         </div>
-      </div>
 
     </section>
   )
