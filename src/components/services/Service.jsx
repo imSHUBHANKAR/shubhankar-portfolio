269091d56541229a4bb419a818a6efd03e320661
SHUBHANKAR KUMAR <shubhankar.kr24@gmail.com>
2024-06-22 18:01:54 +0530

    thinking hand commit

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -4,12 +4,13 @@ import {BiCheck} from 'react-icons/bi'
 
 const Service = () => {
   return (
-    <section id='services'>
+    <section id="services">
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
       <div className="container services__container">
-        <article className='service'>
+        {/* --------------------------------1st sections -------------------------------------------------*/}
+        <article className="service">
           <div className="services__head">
             <h3>UI/UX Design</h3>
             <span>
@@ -24,40 +25,40 @@ const Service = () => {
             </span>
           </div>
 
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>User Interface (UI) Design Services</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Wireframe</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Typography</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Prototype</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Color Combination</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Case Study</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Mobile First Responsive Design Services</p>
             </li>
           </ul>
         </article>
         {/* END OF UI/UX Design */}
-
-        <article className='service'>
+        {/* --------------------------------2nd sections -------------------------------------------------*/}
+        <article className="service">
           <div className="services__head">
             <h3>Web Development</h3>
             <div class="hand">
@@ -70,40 +71,40 @@ const Service = () => {
             </div>
           </div>
 
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Website Design.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Front-End Development.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Back-End Development.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Custom Website Development.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Mobile-Responsive Design.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>API Development.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Website Maintenance and Support.</p>
             </li>
           </ul>
         </article>
         {/* END OF WEB DEVELOPMENT */}
-
-        <article className='service'>
+        {/* --------------------------------3rd sections -------------------------------------------------*/}
+        <article className="service">
           <div className="services__head">
             <h3>Content Creation</h3>
             <span>
@@ -116,45 +117,43 @@ const Service = () => {
                 <span class="palm"></span>
               </div>
             </span>
-
           </div>
 
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Blog Post Writing.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Website Content Writing.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Social Media Content.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Product Descriptions.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Academic Writing</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Health and Wellness Content</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <BiCheck className="service__list-icon" />
               <p>Food and Recipe Writing</p>
             </li>
           </ul>
         </article>
         {/* END OF CONTENT CREATION */}
-
       </div>
     </section>
-  )
+  );
 }
 
 export default Service
