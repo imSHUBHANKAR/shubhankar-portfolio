fec3420c00cdd6018d5543a96890ad55bd6eb6ea
SHUBHANKAR KUMAR <shubhankar.kr24@gmail.com>
2024-07-26 23:06:41 +0530

    about section commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import {Link} from 'react-scroll'
 import "./about.css";
 import ME from "../../assets/aboutPic.jpg";
+import DogPic from '../../assets/DogPic.png'
 // import {FcApproval} from 'react-icons/fc'
 // import {FcOpenedFolder} from 'react-icons/fc'
 // import {FcCustomerSupport} from 'react-icons/fc'
@@ -14,6 +15,23 @@ const About = () => {
       <h2>About Me</h2>
 
       <div className="container about__container">
+        {/* -----------------------Dog Pic--------------- */}
+        <div className="portfolio-card">
+          <div className="portfolio-content">
+            <div className="dog-image">
+              <img src={DogPic} alt="Dog" />
+            </div>
+            <div className="portfolio-details">
+              <h2>My Portfolio</h2>
+              <p className="portfolio-amount">$243,594.80</p>
+              <div className="portfolio-growth">
+                <div className="growth-icon">📊</div>
+                <p>+25%</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* ------------------------------------------------------- */}
         <div className="about__me">
           <div className="about__me-image">
             <img src={ME} alt="About Pic" />
@@ -23,13 +41,13 @@ const About = () => {
         <div className="experience__container">
           <div className="experience__frontend">
             <h3>Read plz</h3>
-              <p style={{ margin: "10px" }}>
-                Highly motivated Frontend and UI/UX Developer with a strong
-                foundation in computer science. Committed to continuous learning
-                and teamwork, specializing in web and app development. Skilled
-                in creative arts for idea generation, adept at multitasking, and
-                delivering innovative solutions in collaborative environments.
-              </p>
+            <p style={{ margin: "10px" }}>
+              Highly motivated Frontend and UI/UX Developer with a strong
+              foundation in computer science. Committed to continuous learning
+              and teamwork, specializing in web and app development. Skilled in
+              creative arts for idea generation, adept at multitasking, and
+              delivering innovative solutions in collaborative environments.
+            </p>
           </div>
         </div>
       </div>
--- src/components/aboutUs/About.jsx
+++ src/components/aboutUs/About.jsx
@@ -6,6 +6,8 @@ import { FaGithub } from "react-icons/fa";
 import { FcDribbble } from "react-icons/fc";
 import { FaLinkedinIn } from "react-icons/fa";
 import { BsBehance } from "react-icons/bs";
+import DogPic from "../../assets/DogPic.png";
+import Cat from '../../assets/cat.png';
 
 const About = () => {
   return (
@@ -14,8 +16,62 @@ const About = () => {
       <h2>About Me</h2>
 
       <div className="container portfolio__container">
+        {/* -----------------------Dog Pic--------------- */}
+        <div className="portfolio-card">
+          <div className="portfolio-card-content">
+            <div className="dog-image">
+              <img src={DogPic} alt="Dog" />
+            </div>
+            <div className="portfolio-card-details">
+              {/* <article className="portfolio__item "> */}
+              <div className="portfolio-card__item-image  ">
+                <img src={ME} alt="Profile_Pic" />
+              </div>
+              <div className="s">
+                <h3>Social Links</h3>
+                <div className="portfolio-card__social_links">
+                  <a
+                    className="linkedin"
+                    href="https://www.linkedin.com/in/mrshubhankar/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <FaLinkedinIn size={32} />
+                  </a>
+                  <a
+                    className="github"
+                    href="https://github.com/imSHUBHANKAR"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <FaGithub size={32} />
+                  </a>
+                  <a
+                    className="dribbble"
+                    href="https://dribbble.com/Shubh242"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <FcDribbble size={32} />
+                  </a>
+                  <a
+                    className="benhance"
+                    href="https://www.behance.net/mr-shubhankar"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <BsBehance size={32} />
+                  </a>
+                </div>
+              </div>
+
+              <div className="portfolio__item-cts"></div>
+            </div>
+          </div>
+        </div>
+        {/* -----------------------Dog Pic--------------- */}
         {/* --------------------------------1st sections -------------------------------------------------*/}
-        <article className="portfolio__item ">
+        {/* <article className="portfolio__item ">
           <div className="portfolio__item-image">
             <img src={ME} alt="Profile_Pic" />
           </div>
@@ -58,9 +114,27 @@ const About = () => {
           </div>
 
           <div className="portfolio__item-cts"></div>
-        </article>
+        </article> */}
 
         {/* --------------------------------2nd sections -------------------------------------------------*/}
+        {/* <div className="portfolio-card">
+          <div className="portfolio-card-content">
+            <div className="dog-image">
+              <img src={DogPic} alt="Dog" />
+            </div>
+            <div className="portfolio-card-details">
+              <p style={{ margin: "10px" }}>
+                Highly motivated Frontend and UI/UX Developer with a strong
+                foundation in computer science. Committed to continuous learning
+                and teamwork, specializing in web and app development. Skilled
+                in creative arts for idea generation, adept at multitasking, and
+                delivering innovative solutions in collaborative environments.
+              </p>
+              <div className="portfolio__item-cts"></div>
+            </div>
+          </div>
+        </div> */}
+
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <p style={{ margin: "10px" }}>
@@ -71,6 +145,7 @@ const About = () => {
               delivering innovative solutions in collaborative environments.
             </p>
           </div>
+          <img src={Cat}  style={{width:'15rem', height:'15rem'}} alt="alt"/>
           <div className="portfolio__item-cts"></div>
         </article>
       </div>
